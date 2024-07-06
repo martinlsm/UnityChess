2b8e94b4753a22606afee0f669cd1ca507d85c52
Martin Lindström <martin.lindstrom94@gmail.com>
2024-07-06 23:50:38 +0200

    Move piece back in case it was dragged outside the board

--- Assets/PiecePrefab.cs
+++ Assets/PiecePrefab.cs
@@ -22,6 +22,7 @@ public class PiecePrefab : MonoBehaviour
     public Sprite blackKingSprite;
 
     private bool isDragged = false;
+    private Vector3 lastValidPos;
 
     public void Init(Piece piece) {
         if (piece.color == PieceColor.White) {
@@ -67,24 +68,39 @@ public class PiecePrefab : MonoBehaviour
                     break;
             }
         }
+
+        var p = gameObject.transform.position;
+
+        lastValidPos = new Vector3(p.x, p.y, p.z);
     }
 
     void OnMouseDrag() {
         float zVal = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
      	Vector3 dest = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, zVal ));
-        transform.position = new Vector3( dest.x, dest.y, dest.z );
+        transform.position = new Vector3(dest.x, dest.y, dest.z);
         isDragged = true;
     }
 
     public void OnMouseUp() {
         if (isDragged) {
             var currPos = transform.position;
-            transform.position = new Vector3(round(currPos.x), round(currPos.y), currPos.z);
+            var tileX = round(currPos.x);
+            var tileY = round(currPos.y);
+
+            if (tileX >= 0 && tileX < Constants.BOARD_WIDTH && tileY >= 0 && tileY < Constants.BOARD_HEIGHT) {
+                lastValidPos = new Vector3(round(currPos.x), round(currPos.y), currPos.z);
+            }
+
+            transform.position = lastValidPos;
         }
         isDragged = false;
     }
 
     static int round(double d) {
-        return (int)(d + 0.5);
+        if (d >= 0) {
+            return (int)(d + 0.5);
+        } else {
+            return (int)(d - 0.5);
+        }
     }
 }
